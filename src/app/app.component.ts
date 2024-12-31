b559e5cb44bdc37b258d7ce0843e8a21756d24f6
Samir Pandey <samee.pan1994@gmail.com>
2024-12-31 10:40:08 -0600

    restructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   title = 'Owners Hub';
-  inputValue: string = '';
-  
-  // errorMessage = signal('');
-  showError: boolean = false;
-
-  onSubmit() {
-    if (!this.inputValue) {
-      // If input is empty, show error message
-      this.showError = true;
-    } else {
-      // Otherwise, reset the error message
-      this.showError = false;
-      console.log('Form submitted successfully with pin:', this.inputValue);
-      // You can handle the form submission here, e.g., send to an API
-    }
-  }
 
 }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../footer/footer.component";
+import { NewUserComponent } from "../new-user/new-user.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FooterComponent, NewUserComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
